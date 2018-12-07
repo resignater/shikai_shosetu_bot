0705080cd0f80b96121cc1a1c5d760bc9efc1407
resignater <chinesecharacters2@gmail.com>
2018-12-07 21:39:37 +0900

    typo修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -92,6 +92,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:content, :url, ;rtid)
+      params.require(:article).permit(:content, :url, :rtid)
     end
 end
